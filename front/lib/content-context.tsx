3f8064db1693e060548b338a67329b39b26c1f82
thiagoulloa <tafarelulloa@gmail.com>
2026-05-08 11:36:33 -0300

    Add ContextProvider and NavBar components; update layout and landing structure

---
+++ front/lib/content-context.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+
+type ContentContextProviderProps = {
+  children: ReactNode;
+};
+
+type ContentContextValue = {
+  walletAddress: string | null;
+  connectWallet: () => Promise<void>;
+  disconnectWallet: () => Promise<void>;
+};
+
+const ContentContext = createContext<ContentContextValue>({
+  walletAddress: null,
+  connectWallet: async () => {},
+  disconnectWallet: async () => {},
+} as ContentContextValue);
+
+export function useContentContext() {
+  const context = useContext(ContentContext);
+
+  if (!context) {
+    throw new Error("useContentContext must be used within a ContextProvider");
+  }
+
+  return context;
+}
+
+export function ContextProvider({ children }: ContentContextProviderProps) {
+  const [walletAddress, setWalletAddress] = useState<string>("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { solana } = window as any;
+
+    try {
+      if (!solana) {
+        alert("Phantom wallet not found.");
+        return;
+      }
+
+      if (solana.isPhantom) {
+        const response = await solana.connect({ onlyIfTrusted: true });
+
+        setWalletAddress(response.publicKey.toString());
+      }
+    } catch (error) {
+      console.error("Error checking wallet connection:", error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window as any;
+
+    try {
+      if (!solana) {
+        return;
+      }
+
+      const response = await solana.connect();
+
+      setWalletAddress(response.publicKey.toString());
+    } catch (error) {
+      console.error("Error connecting wallet:", error);
+    }
+  };
+
+  const disconnectWallet = async () => {
+    const { solana } = window as any;
+
+    try {
+      if (!solana) {
+        return;
+      }
+      await solana.disconnect();
+      setWalletAddress("");
+    } catch (error) {
+      console.error("Error disconnecting wallet:", error);
+    }
+  };
+
+  useEffect(() => {
+    void checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <ContentContext.Provider
+      value={{ walletAddress, connectWallet, disconnectWallet }}
+    >
+      {children}
+    </ContentContext.Provider>
+  );
+}
